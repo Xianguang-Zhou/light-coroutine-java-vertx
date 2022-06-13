2504421d2c74af81289c2caea7cc92de486b12ae
Xianguang-Zhou <xianguang.zhou@outlook.com>
2022-06-13 12:39:23 +0800

    Improve.

--- light-coroutine-java-vertx-core/src/main/java/pers/zxg/coroutine/vertx/core/VertxCoroutine.java
+++ light-coroutine-java-vertx-core/src/main/java/pers/zxg/coroutine/vertx/core/VertxCoroutine.java
@@ -5,8 +5,11 @@ package pers.zxg.coroutine.vertx.core;
 
 import java.util.concurrent.ExecutionException;
 
+import io.vertx.core.Context;
 import io.vertx.core.Future;
+import pers.zxg.coroutine.Coroutine.Callable;
 import pers.zxg.coroutine.Coroutine.OutsideCall;
+import pers.zxg.coroutine.Coroutine.Runnable;
 import pers.zxg.coroutine.Coroutine.Suspend;
 import pers.zxg.coroutine.Coroutine.Weaver.NotWeaved;
 
@@ -19,6 +22,14 @@ public class VertxCoroutine {
 	}
 
 	public static <T> T await(Future<T> future) throws Suspend, ExecutionException, OutsideCall, OutOfVertxContexts {
+		throw new NotWeaved();
+	}
+
+	public static java.util.concurrent.Future<Void> run(Runnable runnable, Context context) {
+		throw new NotWeaved();
+	}
+
+	public static <T> java.util.concurrent.Future<T> run(Callable<T> callable, Context context) {
 		throw new NotWeaved();
 	}
 }
